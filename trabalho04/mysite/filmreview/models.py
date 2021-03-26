dda2f430e0a8349d24c2eb7df24bbf89d3a9fd54
Vitor Bardasson <vbardasson@id.uff.br>
2021-03-26 19:54:42 -0300

    Protótipos de views criadas; BD populado

--- trabalho04/mysite/filmreview/models.py
+++ trabalho04/mysite/filmreview/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 
 class Movie(models.Model):
+    
+    def __str__(self):
+        return self.original_name
+
     original_name = models.CharField(max_length=100)
     director_name = models.CharField(max_length=50)
     movie_genre = models.CharField(max_length=20)
+    release_year = models.CharField(max_length=4)
     country = models.CharField(max_length=30)
     avg_imdb = models.FloatField()
+    sinopsis = models.TextField()
 
 class Review(models.Model):
+    
+    def __str__(self):
+        return self.review_title
+    
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     original_source = models.URLField()
     review_title = models.CharField(max_length=40)
     review_text = models.TextField()
+    avaliation = models.FloatField()
--- trabalho04/mysite/filmreview/views.py
+++ trabalho04/mysite/filmreview/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(req):
+    pass
+
+def review(req, movie_id):
+    pass
