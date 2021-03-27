c2eac7b5c2f9a3ac951d22fc6376a8376b05b272
Vitor Bardasson <vbardasson@id.uff.br>
2021-03-26 23:17:19 -0300

    Template review.html

--- trabalho04/mysite/filmreview/models.py
+++ trabalho04/mysite/filmreview/models.py
@@ -3,23 +3,23 @@ from django.db import models
 class Movie(models.Model):
     
     def __str__(self):
-        return self.original_name
+        return self.original_title
 
-    original_name = models.CharField(max_length=100)
+    original_title = models.CharField(max_length=100)
     director_name = models.CharField(max_length=50)
-    movie_genre = models.CharField(max_length=20)
+    genre = models.CharField(max_length=20)
     release_year = models.CharField(max_length=4)
     country = models.CharField(max_length=30)
     avg_imdb = models.FloatField()
-    sinopsis = models.TextField()
+    synopsis = models.TextField(default="")
 
 class Review(models.Model):
     
     def __str__(self):
-        return self.review_title
+        return self.title
     
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     original_source = models.URLField()
-    review_title = models.CharField(max_length=40)
-    review_text = models.TextField()
-    avaliation = models.FloatField()
+    title = models.CharField(max_length=40)
+    text = models.TextField()
+    avaliation = models.FloatField(null=False)
--- trabalho04/mysite/filmreview/views.py
+++ trabalho04/mysite/filmreview/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from filmreview.models import Movie, Review
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    highlights = Movie.objects.all()[:2]
+    print(highlights)
+    previous_reviews = Movie.objects.all()[:len(Movie.objects.all())-2]
+    print(previous_reviews)
+    context = {'highlights': highlights, 'previous_reviews': previous_reviews}
+    return render(request, 'index.html', context)
 
 def review(request, movie_id):
-    pass
+    movie = Movie.objects.get(pk=movie_id)
+    review = Review.objects.get(movie_id=movie_id)
+    context = {'movie': movie, 'review': review}
+    return render(request, 'review.html', context)
