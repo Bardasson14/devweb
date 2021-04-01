f2612e490ff034c992a753477d1d96c0ac791d89
Vitor Bardasson <vbardasson@id.uff.br>
2021-04-01 14:27:03 -0300

    Projeto final do hortifruti incorporado ao repositório

---
+++ trabalho05/autenticacao/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect, get_object_or_404
+
+from autenticacao.forms import UsuarioFormCustomizado
+from projeto import settings
+
+
+def registra_usuario(request):
+    if request.method == 'POST':
+        form = UsuarioFormCustomizado(request.POST)
+
+        if form.is_valid():
+            novo_usuario = form.save()
+            request.session['usuario_id'] = novo_usuario.id
+
+            senha = form['password1'].value() # request.POST.get('password1')
+            subject = 'Criação de Conta no Hortifruti.'
+            message = novo_usuario.first_name + ' ' + novo_usuario.last_name + \
+                ", \n\nSeguem os dados da sua conta no Hortifruti: " + \
+                "\nConta: " +novo_usuario.username + \
+                "\nSenha: " + senha + \
+                "\n\nAtenciosamente, " + \
+                "\nO Seu Hortifruti."
+            from_email = settings.EMAIL_HOST_USER
+            to_list = [novo_usuario.email]
+
+            send_mail(subject, message, from_email, to_list)
+            return redirect('autenticacao:exibe_usuario')
+    else:
+        form = UsuarioFormCustomizado()
+
+    return render(request, 'autenticacao/registra_usuario.html', {'form': form})
+
+def exibe_usuario(request):
+    usuario = get_object_or_404(User, pk=request.session['usuario_id'])
+    return render(request, 'autenticacao/exibe_usuario.html', {'usuario': usuario})
