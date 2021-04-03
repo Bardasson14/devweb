fc7de0e465e33fcf9fe589d3a2fb712ed789f031
Vitor Bardasson <vbardasson@id.uff.br>
2021-04-02 22:15:27 -0300

    Limpeza no código fornecedores/admin.py

--- trabalho05/fornecedor/admin.py
+++ trabalho05/fornecedor/admin.py
@@ -3,11 +3,5 @@ from .models import Fornecedor
 
 class FornecedorAdmin(admin.ModelAdmin):
     fields = ('cnpj', 'nome', 'telefone', 'endereco')
-    
-    #list_display = ['nome', 'slug', 'categoria', 'imagem', 'qtd_estoque', 'preco', 'disponivel']
-    ##search_fields = ['nome', 'imagem']
-    #list_filter = ['categoria']
-    #list_editable = ['categoria', 'imagem', 'qtd_estoque', 'preco', 'disponivel']
-    #prepopulated_fields = {'slug': ('nome',)}
 
 admin.site.register(Fornecedor, FornecedorAdmin)
